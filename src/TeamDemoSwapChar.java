d7a28a8bea2e2a9ffcc145a1913bad09e0902690
jmalue <jrod2416@gmail.com>
2020-01-05 15:17:30 -0500

    first attempt on team demo

---
+++ src/TeamDemoSwapChar.java
@@ -0,0 +1,9 @@
+
+public class TeamDemoSwapChar {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/TeamDemoSwapCharTest.java
@@ -0,0 +1,12 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TeamDemoSwapCharTest {
+
+	@Test
+	void test() {
+		assertEquals("tema", "team");
+	}
+
+}
